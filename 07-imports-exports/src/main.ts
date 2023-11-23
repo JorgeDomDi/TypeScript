4183b6746843c653e4494c8484e81ad6514652ac
Jorge Martin Dominguez Diaz <jorgemartindominguezdiaz@gmail.com>
2023-11-23 00:06:09 -0300

    imports-exports

---
+++ 07-imports-exports/src/main.ts
@@ -0,0 +1,13 @@
+import './style.css'
+import "./topics/07-imports-exports"
+
+// import './topics/01-basic-types'
+//?con el import estamos requiriendo un modulo que es una agrupacion encapsulada de un procedimiento
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  Hola mundo
+`
+
+
+
+console.log("Hola mundo")
