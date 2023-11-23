cc29ffcb1dc7d1783681da38ff33bfc6aa74e73f
Jorge Martin Dominguez Diaz <jorgemartindominguezdiaz@gmail.com>
2023-11-22 23:54:50 -0300

    destructuring functions

---
+++ 06-function-destructuring/src/topics/06-functions-destructuring.ts
@@ -0,0 +1,53 @@
+interface Producto{
+    description:string;
+    price:number;
+
+}
+const phone:Producto={
+    description:"Nokia A1",
+    price:150.5
+}
+const tablet:Producto={
+    description:"iPad Air",
+    price:250.5
+}
+interface TaxCalculationOptions{
+    tax:number;
+    products:Producto[];
+}
+
+function taxCalculation({tax,products}:TaxCalculationOptions):number[]{
+let total=0;
+products.forEach(({price})=>{
+ 
+    total+=price;
+})
+
+return[total,(total*tax)]
+}
+
+const shoppingCard=[phone,tablet];
+const tax=0.15;
+
+const [total,taxTotal]=taxCalculation({
+    products:shoppingCard,
+    tax:0.15
+
+
+})
+
+console.log("Total ",total);
+console.log("Tax ",taxTotal);
+
+
+
+
+
+
+
+
+
+
+
+
+export{}
